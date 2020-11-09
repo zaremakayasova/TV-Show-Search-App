a8b9856b8c6d2b3eb1df7506dcb7e8adddc265aa
Zarema <zaremakayasova@gmail.com>
2020-11-09 18:17:41 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('#searchForm');
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const container = document.createElement("div");
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            container.append(img);
+            container.style.display = "inline-block";
+            document.body.append(container);
+        }
+    }
+}
+
+
